1506f52cc856c9d966235a09d0b0a6fe01e77ef5
zhzlatkov <zhzlatkov@gmail.com>
2022-08-05 13:41:27 +0300

    without Cart functionality

--- src/filters/companies.js
+++ src/filters/companies.js
@@ -1,6 +1,28 @@
-import { getElement } from '../utils.js';
-import display from '../displayProducts.js';
+import { getElement } from "../utils.js";
+import display from "../displayProducts.js";
 
-const setupCompanies = () => {};
+const setupCompanies = (store) => {
+  let companies = ["all", ...new Set(store.map((product) => product.company))];
+  const companiesDOM = getElement(".companies");
+  companiesDOM.innerHTML = companies
+    .map((company) => {
+      return `<button class="company-btn">${company}</button>`;
+    })
+    .join("");
+  companiesDOM.addEventListener("click", (e) => {
+    const element = e.target;
+    if (element.classList.contains("company-btn")) {
+      let newStore = [];
+      if (element.textContent === "all") {
+        newStore = [...store];
+      } else {
+        newStore = store.filter(
+          (product) => product.company === element.textContent
+        );
+      }
+      display(newStore, getElement(".products-container"));
+    }
+  });
+};
 
 export default setupCompanies;
