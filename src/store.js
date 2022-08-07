a0f4acfd7233a50d1f0643e51742df374a4da3b4
zhzlatkov <zhzlatkov@gmail.com>
2022-08-07 09:19:23 +0300

    with full cart functionality and some bugs to fix

--- src/store.js
+++ src/store.js
@@ -11,5 +11,8 @@ const setupStore = (products) => {
   });
   setStorageItem("store", store);
 };
-const findProduct = () => {};
+const findProduct = (id) => {
+  let product = store.find((product) => product.id === id);
+  return product;
+};
 export { store, setupStore, findProduct };
